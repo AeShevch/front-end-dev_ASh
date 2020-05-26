afe9c52a45ef9663b4ff03783b47265bb48dae90
AeShevch <shevch.ae@gmail.com>
2020-05-27 02:38:23 +0300

    feat: fills tab s WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,65 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css'),
-    sourcemaps = require('gulp-sourcemaps'),
-    del = require('del'),
-    concat = require('gulp-concat'),
-    plumber = require('gulp-plumber'),
-    livereload = require('gulp-livereload');
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  cleanCSS = require("gulp-clean-css"),
+  sourcemaps = require("gulp-sourcemaps"),
+  del = require("del"),
+  concat = require("gulp-concat"),
+  plumber = require("gulp-plumber"),
+  livereload = require("gulp-livereload");
 
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
-const webpackConfig = require('./webpack.config.js');
+const webpack = require("webpack");
+const webpackStream = require("webpack-stream");
+const webpackConfig = require("./webpack.config.js");
 
 function scss() {
-    return gulp.src(
-        [
-            'src/scss/*.scss'
-        ])
-        .pipe(sourcemaps.init())
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({
-            level: {
-                1: {
-                    specialComments: 0
-                }
-            }
-        }))
-        .pipe(concat("bundle.css"))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(livereload());
+  return gulp
+    .src(["src/scss/*.scss"])
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(
+      cleanCSS({
+        level: {
+          1: {
+            specialComments: 0,
+          },
+        },
+      })
+    )
+    .pipe(concat("bundle.css"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/css"))
+    .pipe(livereload());
 }
 
-const js = () => (
-  gulp.src('src/js/App.js')
+const js = () =>
+  gulp
+    .src("src/js/App.js")
     .pipe(webpackStream(webpackConfig), webpack)
-    .pipe(gulp.dest('./dist/js'))
-);
+    .pipe(gulp.dest("./dist/js"));
 
 function html() {
-    return gulp.src('index.html')
-        .pipe(livereload());
+  return gulp.src("index.html").pipe(livereload());
 }
 
 function clean() {
-    return del(['dist/css/style.css'])
+  return del(["dist/css/style.css"]);
 }
 
-gulp.task('scss', scss);
-gulp.task('html', html);
+gulp.task("scss", scss);
+gulp.task("html", html);
 
 function watch() {
-    livereload.listen();
-    gulp.watch('src/scss/*.scss', scss);
-    gulp.watch('index.html', html);
-    gulp.watch('src/js/*.js', js);
+  livereload.listen();
+  gulp.watch("src/scss/*.scss", scss);
+  gulp.watch("index.html", html);
+  gulp.watch("src/js/*.js", js);
 }
 
-gulp.task('watch', watch);
+gulp.task("watch", watch);
 
-gulp.task('build', gulp.series(
-    clean,
-    gulp.parallel(scss)
-));
+gulp.task("build", gulp.series(clean, gulp.parallel(scss, js)));
 
-gulp.task('default', gulp.series('build', 'watch'));
+gulp.task("default", gulp.series("build", "watch"));
--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,22 @@
 import Tabs from "./Tabs";
+import Data from "./Data";
 
-const configuratorTabs = new Tabs('.js-tabs-container');
+const JSON_URL = "https://www.iport.ru/upload/front/data.json";
+
+// Initializing tabs
+const configuratorTabs = new Tabs(".js-tabs-container");
 configuratorTabs.init();
+
+// Receiving data
+const data = new Data(JSON_URL);
+data.receive().then((data) => {
+  console.log(data);
+
+  const tabTypeFragment = document.createDocumentFragment();
+  const tabPropsFragment = document.createDocumentFragment();
+  const tabSetFragment = document.createDocumentFragment();
+
+  data.forEach((prop) => {
+
+  });
+});
