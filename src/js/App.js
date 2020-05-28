7b46480e33c32544a074b06ad6e17a7818a4a3da
AeShevch <Neron610>
2020-05-28 14:37:23 +0300

    doc: Updates comments

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 /**
- * TODO Изначально планировалось, что json будет браться аяксом с сервера,
+ * (*) TODO Изначально планировалось, что json будет браться аяксом с сервера,
  * Поэтому компонент Data получает json и возвращает promise,
  * Но сейчас получение данных закомментировано и компонент просто отдаёт json
  * Как будто это проект в прод и, как бэк подготовит сервер, мы раскомментируем
@@ -9,14 +9,14 @@ import Tabs from "./Tabs";
 import Data from "./Data";
 import Form from "./Form";
 
-// Url for fetch *
+// Url for fetch (*)
 const JSON_URL = "https://www.iport.ru/upload/front/data.json";
 
 // Initializing tabs
 const configuratorTabs = new Tabs(".js-tabs-container");
 configuratorTabs.init();
 
-// Receiving data *
+// Receiving data and filling tabs (*)
 new Data(JSON_URL).receive().then((data) => configuratorTabs.fill(data));
 
 // Initializing form
