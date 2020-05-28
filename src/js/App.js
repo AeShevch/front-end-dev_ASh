284c80303e6f5d0c5d7dd79c26a6fcad5c170b38
AeShevch <Neron610>
2020-05-28 14:17:30 +0300

    refactor: Moves tabs filling to Tab component

--- src/js/App.js
+++ src/js/App.js
@@ -1,16 +1,12 @@
 import Tabs from "./Tabs";
 import Data from "./Data";
 
-import Spoiler from "./Spoiler";
-
 const JSON_URL = "https://www.iport.ru/upload/front/data.json";
 
 // Initializing tabs
 const configuratorTabs = new Tabs(".js-tabs-container");
 configuratorTabs.init();
-const tabTypeElement = document.querySelector(".js-tab-type");
-const tabPropsElement = document.querySelector(".js-tab-props");
-const tabSetElement = document.querySelector(".js-tab-set");
+
 
 // Receiving data
 // TODO Изначально планировалось, что json будет браться аяксом с сервера,
@@ -18,37 +14,7 @@ const tabSetElement = document.querySelector(".js-tab-set");
 // Но сейчас получение данных закомментировано и компонент просто отдаёт json
 // Как будто это проект в прод и как бэк подготовит сервер, мы раскомментируем
 const data = new Data(JSON_URL);
-data.receive().then((data) => {
-  // Filling form tabs
-  // Creating tabs fragments
-  const tabTypeFragment = document.createDocumentFragment();
-  const tabPropsFragment = document.createDocumentFragment();
-  const tabSetFragment = document.createDocumentFragment();
-
-  // Rendering spoilers, filling the fragments
-  data.forEach((prop) => {
-    const spoilerHtml = new Spoiler().render(prop);
-
-    // Inserting elements in fragments
-    switch (prop.code) {
-      case "model":
-        tabTypeFragment.append(spoilerHtml);
-        break;
-      case "mouse":
-      case "keyboard":
-        tabSetFragment.append(spoilerHtml);
-        break;
-      default:
-        tabPropsFragment.append(spoilerHtml);
-        break;
-    }
-  });
-
-  // Inserting fragments in to tabs
-  tabTypeElement.appendChild(tabTypeFragment);
-  tabPropsElement.appendChild(tabPropsFragment);
-  tabSetElement.appendChild(tabSetFragment);
-});
+data.receive().then((data) => configuratorTabs.fill(data));
 
 const formElement = document.forms["constructor-form"];
 const resultsElement = document.querySelector(".js-form-results");
--- src/js/Tabs.js
+++ src/js/Tabs.js
@@ -1,4 +1,9 @@
 // CSS class-modificator of active tab button
+import Spoiler from "./Spoiler";
+
+/**
+ * Components configurations
+ */
 const ACTIVE_TAB_CONTROL_MOD = "c-tabs__switch_active";
 // CSS class-modificator of active tab
 const ACTIVE_TAB_MOD = "c-tab__link--active";
@@ -6,18 +11,59 @@ const ACTIVE_TAB_MOD = "c-tab__link--active";
 const TAB_BTNS_SELECTOR = ".js-open-tab";
 // CSS selector of tabs
 const TABS_SELECTOR = ".js-tab-content";
+// CSS selector of type tab (first)
+const TABS_TYPE_SELECTOR = ".js-tab-type";
+// CSS selector of properties tab (second)
+const TABS_PROPS_SELECTOR = ".js-tab-props";
+// CSS selector of set tab (third)
+const TABS_SET_SELECTOR = ".js-tab-set";
 
 export default class Tabs {
   constructor(containerSelector) {
     this._container = document.querySelector(containerSelector);
     this._controlBtns = this._container.querySelectorAll(TAB_BTNS_SELECTOR);
     this._contentBlocks = this._container.querySelectorAll(TABS_SELECTOR);
+
+    this._tabType = this._container.querySelector(TABS_TYPE_SELECTOR);
+    this._tabProps = this._container.querySelector(TABS_PROPS_SELECTOR);
+    this._tabSet = this._container.querySelector(TABS_SET_SELECTOR);
   }
 
   init() {
     this._controlBtns.forEach((controlBtn) => {
       controlBtn.addEventListener("click", this._openTab.bind(this));
     });
+  }
+
+  fill(data) {
+    // Creating tabs fragments
+    const tabTypeFragment = document.createDocumentFragment();
+    const tabPropsFragment = document.createDocumentFragment();
+    const tabSetFragment = document.createDocumentFragment();
+
+    // Rendering spoilers, filling the fragments
+    data.forEach((prop) => {
+      const spoilerHtml = new Spoiler().render(prop);
+
+      // Inserting elements in fragments
+      switch (prop.code) {
+        case "model":
+          tabTypeFragment.append(spoilerHtml);
+          break;
+        case "mouse":
+        case "keyboard":
+          tabSetFragment.append(spoilerHtml);
+          break;
+        default:
+          tabPropsFragment.append(spoilerHtml);
+          break;
+      }
+    });
+
+    // Inserting fragments in to tabs
+    this._tabType.appendChild(tabTypeFragment);
+    this._tabProps.appendChild(tabPropsFragment);
+    this._tabSet.appendChild(tabSetFragment);
   }
 
   _openTab(evt) {
